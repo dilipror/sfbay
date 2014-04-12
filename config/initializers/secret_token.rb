47ac58f0a2247476edaecd48c9b8c54ca440bc81
Dipak <dipakg.ror@gmail.com>
2014-04-12 13:05:18 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sfbay::Application.config.secret_key_base = 'f6221b54609a7081dbdcf953b0a2fbc81e5459c10e102b31443607db9472afbe8375f6539d76ee7c44df9f918186563ed47dc6662ec1b04e0b7df6254c8aabc3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sfbay::Application.config.session_store :cookie_store, key: '_sfbay_session'
